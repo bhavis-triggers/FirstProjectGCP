e4d13913382e4b61cf952b3b24a935fd449d5642
bhavis-triggers <bhavithraselvaraj73@gmail.com>
2025-11-01 11:47:15 +0530

    last modified file

--- provider.tf
+++ provider.tf
@@ -1,10 +1,27 @@
+#testing out adding tags to bg dataset
 provider "google" {
+  project = var.gcp_project_id
+  region = var.gcp_region
+  credentials = file(var.gcp_svc_key)
+}
+terraform{
+  required_providers{
+    google = {
+      source = "hashicorp/google"
+      version = "~> 6.0"
+    }
+  }
+}
+
+
+#Below are practice material
+/*provider "google" {
   credentials = file(var.gcp_svc_key)
   project    = var.gcp_project_id
   region     = var.gcp_region
 }
 
-/*terraform {
+terraform {
   required_version = ">1.4.0"
   required_providers {
     google = {
